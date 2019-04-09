aedebec741c6102f5cb25ad02e3f3cc8e4d8fad5
0xhedge <0xhedge@protonmail.com>
2019-04-09 05:01:38 +0000

    Add logging functionality

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
+import os
+import logging
+import time
+
+log_file = "volSurface.log"
+with open(log_file, 'w+') as file:
+    pass
+logging.basicConfig(filename=log_file, level=logging.DEBUG)
+logging.disable(logging.DEBUG)
+logging.info("Starting log at " + time.ctime() + "...")
+
 pair = "BTC/USD"
 data_pull_freq = 60
 delimiter = '/'
